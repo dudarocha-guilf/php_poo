661833f9f3a6923534f070aff1641e0bee0287e3
Maria Eduarda <maria.e.rocha23@aluno.senai.br>
2026-04-15 12:11:57 -0300

    ex 16 17 18

---
+++ exemplo18.php
@@ -0,0 +1,94 @@
+<?php
+
+    // Superclasse
+    class Conta
+
+    {
+        //Atributos, Propriedades e Campos
+
+        private $numero;
+        private $saldo;
+        
+        //Método Construtor
+        function __construct($numero, $saldo)
+        {
+            $this->numero = $numero;
+            $this->saldo = $saldo;
+        }
+
+        //Métodos Getters e Setters
+        public function getNumero()
+        {
+            return $this->numero;
+        }
+
+        public function getSaldo()
+        {
+            return $this->saldo;
+        }
+
+        //Métodos
+        function creditar($valor)
+        {
+            $this->saldo = $this->saldo + $valor;
+        }
+
+        function debitar($valor)
+        {
+            $this->saldo = $this->saldo - $valor;
+        }
+
+        function transferir($outraConta, $valor)
+        {
+            if ($this->saldo >= $valor) 
+            {
+                $this->debitar($valor);
+                $outraConta->creditar($valor);
+            }
+        }
+    }
+
+     class Poupanca extends Conta
+    {
+        protected $juros;
+
+        function __construct($numero, $saldo, $juros)
+        {
+            parent::__construct($numero, $saldo);
+            $this->juros = $juros;
+        }
+
+        public function getNumero()
+        {
+            return $this->numero;
+        }
+
+        public function getSaldo()
+        {
+            return $this->saldo;
+        }
+
+        function creditar($valor)
+        {
+            parent::creditar($valor);
+            $this->atualizarJuros();
+        }
+
+        function atualizarJuros()
+        {
+            $this->saldo = $this->saldo * (1 + $this->juros);
+        }
+    }
+
+    $conta = new Conta(1, 150);
+    $conta->creditar(50);
+    $conta->debitar(100);
+
+    echo "Saldo da Conta {$conta->getNumero()}: {$conta->getSaldo()}<br>";
+
+    $poupanca = new Poupanca(2, 150, 0.10);
+    $poupanca->creditar(50);
+    $poupanca->debitar(100);
+    $poupanca->atualizarJuros();
+
+    echo "Saldo da Conta {$poupanca->getNumero()}: {$poupanca->getSaldo()}<br>";
