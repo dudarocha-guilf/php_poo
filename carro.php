896cd993d0024ce0024b2c28d7af27e3663e296e
Maria Eduarda <maria.e.rocha23@aluno.senai.br>
2026-04-17 08:34:13 -0300

    finalizado

--- carro.php
+++ carro.php
@@ -9,31 +9,27 @@ class Carro {
         $this->velocidade = $velocidade;
     }
 
-    public function getVelocidade()
-        {
-            return $this->velocidade;
-        }
+    public function getVelocidade() {
+        return $this->velocidade;
+    }
 
-    public function setVelocidade($novaVelocidade)
-        {
+    public function setVelocidade($novaVelocidade) {
+        if ($novaVelocidade >= 200) {
             $this->velocidade = $novaVelocidade;
-
-            if ($novaVelocidade >= 200) {
-                echo "Você está acima da velocidade.";
-            } else {
-                echo "Continue com esta velocidade.";
-            }
+            echo "Você está acima da velocidade.<br>";
+        } else {
+            echo "Continue com esta velocidade.<br>";
         }
-
+    }
 }
 
 // --- TESTE DO VEÍCULO ---
 $meuCarro = new Carro("Senai-Mobile", 0);
 
 // O desastre: alteração direta sem validação
-$meuCarro->velocidade = 200; // Velocidade de foguete?
-$meuCarro->velocidade = 0;   // Carro andando no tempo?
+$meuCarro->setVelocidade(200); // Velocidade de foguete?
+$meuCarro->getVelocidade(0); // Carro andando no tempo?
 
-echo "Modelo: " . $meuCarro->modelo . "<br>";
-echo "Velocidade atual: " . $meuCarro->velocidade . " km/h";
+echo "Modelo: " . $meuCarro->modelo. "<br>";
+echo "Velocidade atual: " . $meuCarro->getVelocidade() . " km/h";
 ?>
