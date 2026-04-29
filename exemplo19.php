c91f8ed7911bc7c647a180484c7e5b960c9f5a1e
Maria Eduarda <maria.e.rocha23@aluno.senai.br>
2026-04-29 15:17:31 -0300

    lonche

--- exemplo19.php
+++ exemplo19.php
@@ -18,7 +18,7 @@ abstract class FiguraGeometrica{
     public abstract function perimetro();
 }
 class Circunferencia extends FiguraGeometrica{
-    private=$raio;
+    private $raio;
 
     public function __construct($tipo, $raio){
         parent::__construct($tipo);
@@ -34,3 +34,28 @@ class Circunferencia extends FiguraGeometrica{
     }
     
 }
+class Retangulo extends FiguraGeometrica{
+    private $lado1;
+    private $lado2;
+
+    public function __construct($tipo, $lado1, $lado2){
+        parent::__construct($tipo);
+        $this->lado1 = $lado1;
+        $this->lado2 = $lado2;
+    }
+
+    public function area(){
+        return $this->lado1 * $this->lado2;
+    }
+
+    public function perimetro(){
+        return (2 * $this->lado1) + (2 * $this->lado2);
+    }
+}
+
+$circ = new Circunferencia("Circunferencia", 10);
+$circ->printCaracteristicas();
+
+$retangulo = new Retangulo("Retangulo", 10, 20);
+$retangulo->printCaracteristicas();
+
---
+++ exemplo20.php
@@ -0,0 +1,26 @@
+<?php
+
+interface INotificador {
+    public function enviar($destinatario, $mensagem);
+}
+
+//Implementar: E-mail
+class NotificadorEmail  implements INotificador{
+    public function enviar ($destinatario, $mensagem){
+        echo "Email enviado para: {$destinatario}. Mensagem: {$mensagem}.";
+    }
+}
+
+//Implementar: SMS
+class NotificadorSMS implements INotificador{
+    public function enviar($destinatario, $mensagem){
+        echo "SMS enviado para: {$destinatario}. Mensagem: {$mensagem}.";
+    }
+}
+
+//Implementar: Whatsapp
+class NotificadorWhatsapp implements INotificador{
+    public function enviar($destinatario, $mensagem){
+        echo "Whatsapp enviado para: ($destinatario). Mensagem: {$mensagem}."
+    }
+}
